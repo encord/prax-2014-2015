d8ad5b67d62e5dbe0f0d22a293b27ef30c2fb9d8
matej <matej@vrto.sk>
2015-05-29 16:10:30 +0200

    Doplnené o kryptovanie rot13

--- String.js
+++ String.js
@@ -54,4 +54,67 @@ function mix(s1,s2)
 var s1 = 'Hlowrd';
 var s2 = 'el ol';
 var mixx = mix(s1,s2);
-process.stdout.write('mix(): ' + s1+ ' ' + s2 + ' -> ' + mixx + '\n');
+process.stdout.write('mix(): ' + s1 + ' ' + s2 + ' -> ' + mixx + '\n');
+
+/**
+ *Funkcia rot13 ma zaúlohu zakryptovať slovo pomocou algoritmu rot13.
+ *
+ *@param{String} slovo slovo ktoré chceme zakryptovať
+ *@retun{String}
+ */
+function rot13(slovo)
+{
+	var m1 = 'ABCDEFGHIJKLM';
+	var m2 = 'NOPQRSTUVWXYZ';
+	var pismeno;
+	var crypt = '';
+
+	for(var i = 0; i < slovo.length; i++)
+	{
+		pismeno = _zistiPoziciu(slovo[i],m1,m2);
+
+		if(pismeno[0] == 1)
+		{
+			crypt += m2[(pismeno[1])];
+		}
+		else if(pismeno[0] == 2)
+		{
+		 	crypt += m1[(pismeno[1])];
+		}
+		else if(pismeno.length == 0)
+		{
+		 	crypt += slovo[i];
+		}
+
+	}
+	return crypt;	
+}
+
+/**
+ *Funkcia _zistiPoziciu ma zaúlohu zistiť na ktorej pozicii sa písmeno nachádza.
+ *
+ *Funkcia _zistiPoziciu ma zaúlohu zistiť na ktorej pozicii sa písmeno nachádza a v ktorej mape . 
+ *@param{String} písmeno pismeno ktorého chceme zistiť pozíciu
+ *@param{String} m1 mapa1
+ *@param{String} m2 mapa2
+ *@retun{Array<Number>}
+ */
+function _zistiPoziciu(pismeno,m1,m2)
+{
+	var pozicia = [];
+	for(var i = 0;i < m1.length ; i++)
+	{
+		if(pismeno == m1[i])
+		{
+			pozicia.push(1,i);	
+		}
+		else if(pismeno == m2[i])
+		{
+			pozicia.push(2,i);
+		}			
+	}
+	return pozicia;
+}
+var slovo = 'HELLO WORLD!'
+var cr = rot13(slovo);
+process.stdout.write('rot13(): ' + slovo + ' -> '+ cr + '\n');
